abf45db8208f7f759366acb41dcbbef940104568
jolo84 <66883508+jolo84@users.noreply.github.com>
2020-07-12 22:36:29 +0200

    Add files via upload

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -10,21 +10,25 @@ $(window).ready(function(){
 
     $(".timeline-bg").css({"margin-top": timelineBgMarginTop - 7 + "%"})
 
+
     $(window).scroll(function(){
-        if($(document).scrollTop() > $("header").height()){
-            $(".timeline-container").eq(0).animate({"opacity": "1"},"fast");
+        if($(document).scrollTop() > $("header").height()*1.2){
+            $(".timeline-container").eq(0).fadeIn(2000);
+            $("#timeline-circle-1").eq(0).fadeIn(2000);
         }
         let i;
         let iMax = $(".timeline-container").length;
-        console.log(iMax);
         for(i = 1; i <= iMax; ++i ){
             let headerHObj = $("header").css("height");
             let headerH = Number(headerHObj.replace("px", ""));
             let containerHObj = $(".timeline-container").css("height");
             let containerH = Number(containerHObj.replace("px", ""));
-            if($(document).scrollTop() > headerH + (containerH * i) ){
-                $(".timeline-container").eq(i).animate({"opacity": "1"},"fast");
-            }   
+            if($(document).scrollTop() > (headerH + (containerH * i))*1.2 ){
+                $(".timeline-container").eq(i).fadeIn(1000);
+                $(".timeline-circle").eq(i).fadeIn(1000);
+                console.log(i);
+            }
+            console.log(i);
         }
 
     });
