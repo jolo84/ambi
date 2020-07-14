5e8137927a1335ce6a9eadb248ea3ac7385f9a62
jolo84 <66883508+jolo84@users.noreply.github.com>
2020-07-14 18:52:03 +0200

    Add files via upload

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
+
     let contact = function(){
-         
+        $(document).ready(function(){         
         $("#contact").removeClass("hide");
         $("#about-me").addClass("hide");
         $("#service").addClass("hide");
         $(this).addClass("active");
         $("#tab-service").removeClass("active");
         $("#tab-about-me").removeClass("active");
-     
+    });
     }
-});
